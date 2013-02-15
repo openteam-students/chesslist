8aea2fcccd38a870d0436c39386feb1f99ebf922
DeKoniX <dekonix@gmail.com>
2013-02-15 15:12:39 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chesslist::Application.config.secret_token = '64ff979b2eb9f5e8dd3eab59f3a9b63aa2cd40d3c3bd32dcc882a635a2d6062432c43ab44996b3aad17cab4075a84c092b13648835ddb55a4d75562aded845cd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Chesslist::Application.config.session_store :cookie_store, key: '_chesslist_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Chesslist::Application.config.session_store :active_record_store
